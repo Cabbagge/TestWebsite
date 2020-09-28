9e8d75eb9193bbbdab931c0ed5173fddd81440f6
kylej <kylej@LAPTOP-5IM3DINO>
2020-09-28 10:55:41 +0800

    initial upload

---
+++ TestWebsite/src/test/java/com/testwebsite/TestWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
